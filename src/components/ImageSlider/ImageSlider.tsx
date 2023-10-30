8037d10f00383a5a446b2ef3fbd1f41c3921a7e9
Coding-Cooking <fei.coding2023@gmail.com>
2023-10-30 21:38:36 +1100

    add slider in the home page

--- src/components/ImageSlider/ImageSlider.tsx
+++ src/components/ImageSlider/ImageSlider.tsx
@@ -5,35 +5,48 @@ import ImageOne from "../../../public/bgimag/pic1.jpg";
 import ImageTwo from "../../../public/bgimag/pic2.jpg";
 import ImageThree from "../../../public/bgimag/pic3.jpg";
 import ImageFour from "../../../public/bgimag/pic4.jpg";
+import ImageFive from "../../../public/bgimag/pic5.jpg";
+import ImageSix from "../../../public/bgimag/pic6.jpg";
+import ImageSeven from "../../../public/bgimag/pic7.jpg";
 import { useEffect, useState } from 'react';
 import { useMediaQuery } from '@/hooks/useMediaQuery';
 import { breakpoints } from '@/styles/theme';
 import styles from "./ImageSlider.module.scss"
+import 'swiper/css/navigation';
+import { Navigation } from 'swiper/modules';
 
 const ImageSlider = () => {
   const isLargeScreen = useMediaQuery(breakpoints.md);
   const [previewSlides, setPreviewSlides] = useState(1);
 
-  useEffect(() => {
-    if (isLargeScreen){
-      setPreviewSlides(1);
-    } else {
-      setPreviewSlides(1);
-    }
-  }, [isLargeScreen])
+  // useEffect(() => {
+  //   if (isLargeScreen){
+  //     setPreviewSlides(1);
+  //   } else {
+  //     setPreviewSlides(1);
+  //   }
+  // }, [isLargeScreen])
 
   return (
-    <Swiper className={styles.swiper}
-      spaceBetween={50}
-      slidesPerView={previewSlides}
-      onSlideChange={() => console.log('slide change')}
-      onSwiper={(swiper) => console.log(swiper)}
-    >
-      <SwiperSlide className={styles.swiperslide}><Image src={ImageOne} alt='' /></SwiperSlide>
-      <SwiperSlide className={styles.swiperslide}><Image src={ImageTwo} alt='' /></SwiperSlide>
-      <SwiperSlide className={styles.swiperslide}><Image src={ImageThree} alt='' /></SwiperSlide>
-      <SwiperSlide className={styles.swiperslide}><Image src={ImageFour} alt='' /></SwiperSlide>
-    </Swiper>
+    <div className={styles.container}>
+      <Swiper
+        navigation={true} 
+        modules={[Navigation]}
+        // spaceBetween={50}
+        slidesPerView={1}
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        <SwiperSlide><Image src={ImageOne} alt='' /></SwiperSlide>
+        <SwiperSlide><Image src={ImageTwo} alt='' /></SwiperSlide>
+        <SwiperSlide><Image src={ImageThree} alt='' /></SwiperSlide>
+        <SwiperSlide><Image src={ImageFour} alt='' /></SwiperSlide>
+        <SwiperSlide><Image src={ImageFive} alt='' /></SwiperSlide>
+        <SwiperSlide><Image src={ImageSix} alt='' /></SwiperSlide>
+        <SwiperSlide><Image src={ImageSeven} alt='' /></SwiperSlide>
+      </Swiper>
+    </div>
+    
   )
 }
 
--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -4,12 +4,13 @@ import styles from "./about.module.scss"
 export default function About() {
     const { theme, setTheme } = useThemeContext();
 
-
     return <div className={`${styles.container} ${theme === "dark" && styles.darkContainer}`}>
         <div  className={styles.aboutWrapper}>
             <p> I'm a software engineer living in Sydney. </p>
             <p>Thank you for visiting my blog.</p>
             <p>If you have anything want to share with me, please subscribe my blog and discuss with me.</p>
+
+            
         </div>
 
     </div>
